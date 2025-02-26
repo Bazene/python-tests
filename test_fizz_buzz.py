da8a21c766de416e4ae6134ef74294053016e59c
bazene <bazenesergeamos0@gmail.com>
2025-02-26 13:14:27 +0200

    Refactor : fix bug

--- test_fizz_buzz.py
+++ test_fizz_buzz.py
@@ -3,12 +3,12 @@ import pytest
 
 # USE CASE 1 : call FizzBuzz
 def fizz_buzz(value):
+    if value % 15 == 0:
+        return "FizzBuzz"
     if value % 3 == 0:
         return "Fizz"
     if value % 5 == 0:
         return "Buzz"
-    if value % 15 == 0:
-        return "FizzBuzz"
     return str(value)
 
 def test_can_call_fizz_buzz():
@@ -16,7 +16,7 @@ def test_can_call_fizz_buzz():
 
 def check_expected(value, expected_string):
     response = fizz_buzz(value)
-    assert response == "expected_string"
+    assert response == expected_string
 
 # USE CASE 2 : Get "1" when I pass in 1
 def test_returns1_with_1passed():
@@ -24,6 +24,7 @@ def test_returns1_with_1passed():
 
 # USE CASE 3 : Get "Fizz" when I pass in 6 (a multiple of 3)
 def test_fizz_for_3multiples():
+    print("Hey")
     check_expected(3, "Fizz")
 
 # USE CASE 4 : Get "Fizz" when I pass in 6 (a multiple of 3)
@@ -34,4 +35,4 @@ def test_fizz_for_5multiples():
 def test_fizz_for_15multiples():
     check_expected(15, "FizzBuzz")
 
-    test_fizz_for_15multiples()
+test_returns1_with_1passed()
